1ff4d1a390c1b9877f204b4b0797e8d37a2363be
anasMuf <anas.muhammadakbar@gmail.com>
2024-01-17 21:00:39 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html
+  lang="en"
+  class="light-style layout-navbar-fixed layout-wide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="{{ asset('assets/assets/') }}"
+  data-template="front-pages-no-customizer">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Landing Page - Front Pages | Materialize - Material Design HTML Admin Template</title>
+
+    <meta name="description" content="" />
+
+    @stack('prepand-style')
+    @include('layouts.includes.styles')
+    @stack('append-style')
+
+    <!-- Helpers -->
+    <script src="{{ asset('assets/assets/vendor/js/helpers.js') }}"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{ asset('assets/assets/js/front-config.js') }}"></script>
+  </head>
+
+  <body>
+    <script src="{{ asset('assets/assets/vendor/js/dropdown-hover.js') }}"></script>
+    <script src="{{ asset('assets/assets/vendor/js/mega-dropdown.js') }}"></script>
+
+    <!-- Navbar: Start -->
+    @include('layouts.includes.navbar')
+    <!-- Navbar: End -->
+
+    <!-- Sections:Start -->
+
+    @yield('content')
+
+    <!-- / Sections:End -->
+
+    <!-- Footer: Start -->
+    @include('layouts.includes.footer')
+    <!-- Footer: End -->
+
+    <!-- Core JS -->
+    @stack('prepand-script')
+    @include('layouts.includes.scripts')
+    @stack('append-script')
+  </body>
+</html>
---
+++ resources/views/layouts/includes/styles.blade.php
@@ -0,0 +1,28 @@
+<!-- Favicon -->
+<link rel="icon" type="image/x-icon" href="{{ asset('assets/assets/img/favicon/favicon.ico') }}" />
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap"
+  rel="stylesheet" />
+
+<link rel="stylesheet" href="{{ asset('assets/assets/vendor/fonts/materialdesignicons.css') }}" />
+
+<!-- Menu waves for no-customizer fix -->
+<link rel="stylesheet" href="{{ asset('assets/assets/vendor/libs/node-waves/node-waves.css') }}" />
+
+<!-- Core CSS -->
+<link rel="stylesheet" href="{{ asset('assets/assets/vendor/css/rtl/core.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/assets/vendor/css/rtl/theme-default.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/assets/css/demo.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/assets/vendor/css/pages/front-page.css') }}" />
+<!-- Vendors CSS -->
+
+<link rel="stylesheet" href="{{ asset('assets/assets/vendor/libs/nouislider/nouislider.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/assets/vendor/libs/swiper/swiper.css') }}" />
+
+<!-- Page CSS -->
+
+<link rel="stylesheet" href="{{ asset('assets/assets/vendor/css/pages/front-page-landing.css') }}" />
