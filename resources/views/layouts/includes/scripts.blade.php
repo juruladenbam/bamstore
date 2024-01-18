3bf118f51b1efddd97713976a212b0d781657d0c
anasMuf <anas.muhammadakbar@gmail.com>
2024-01-18 12:43:42 +0700

    public path asset

--- resources/views/layouts/includes/scripts.blade.php
+++ resources/views/layouts/includes/scripts.blade.php
@@ -1,17 +1,17 @@
 
 <!-- build:js assets/assets/vendor/js/core.js -->
-<script src="{{ url('/public/assets/assets/vendor/libs/popper/popper.js') }}"></script>
-<script src="{{ url('/public/assets/assets/vendor/js/bootstrap.js') }}"></script>
-<script src="{{ url('/public/assets/assets/vendor/libs/node-waves/node-waves.js') }}"></script>
+<script src="{{ public_path('assets/assets/vendor/libs/popper/popper.js') }}"></script>
+<script src="{{ public_path('assets/assets/vendor/js/bootstrap.js') }}"></script>
+<script src="{{ public_path('assets/assets/vendor/libs/node-waves/node-waves.js') }}"></script>
 
 <!-- endbuild -->
 
 <!-- Vendors JS -->
-<script src="{{ url('/public/assets/assets/vendor/libs/nouislider/nouislider.js') }}"></script>
-<script src="{{ url('/public/assets/assets/vendor/libs/swiper/swiper.js') }}"></script>
+<script src="{{ public_path('assets/assets/vendor/libs/nouislider/nouislider.js') }}"></script>
+<script src="{{ public_path('assets/assets/vendor/libs/swiper/swiper.js') }}"></script>
 
 <!-- Main JS -->
-<script src="{{ url('/public/assets/assets/js/front-main.js') }}"></script>
+<script src="{{ public_path('assets/assets/js/front-main.js') }}"></script>
 
 <!-- Page JS -->
-<script src="{{ url('/public/assets/assets/js/front-page-landing.js') }}"></script>
+<script src="{{ public_path('assets/assets/js/front-page-landing.js') }}"></script>
